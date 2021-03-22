1de5ba03134e4fbfbf55a0115820f1aa36e2a26d
makaramhk <30686963+makaramhk@users.noreply.github.com>
2021-03-22 21:34:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,8 +14,8 @@ curl -F chat_id=$CHAT_ID -F document=@${1} -F parse_mode=markdown https://api.te
 }
 
 sudo apt-get install bc
-MANIFEST=git://github.com/StatiXOS/android_manifest.git
-BRANCH=11
+MANIFEST=git://github.com/AOSiP/platform_manifest.git
+BRANCH=ten
 
 mkdir -p /tmp/rom
 cd /tmp/rom
@@ -35,21 +35,21 @@ tg_sendText "Cloning repo done"
 # Sync device tree and stuffs
 tg_sendText "Cloning Device stuff"
 
-git clone https://github.com/coolhotham/device_lav.git --single-branch -b arrow-11.0 device/xiaomi/lavender --depth=1
-git clone https://github.com/coolhotham/vendor_lav.git --single-branch -b arrow-11.0 vendor/xiaomi/lavender --depth=1
+git clone https://github.com/coolhotham/dev_lav --single-branch -b aosip device/xiaomi/lavender --depth=1
+git clone https://github.com/coolhotham/ven_lav --single-branch -b ten vendor/xiaomi/lavender --depth=1
 git clone https://github.com/NotZeetaa/nexus_kernel_lavender.git -b Hmp kernel/xiaomi/lavender --depth=1
 
 tg_sendText "Done. Cloning HALs...."
 
 #cloning HALs
 # Sync stuffs
-find hardware/qcom-caf/msm8998/display hardware/qcom-caf/msm8998/audio hardware/qcom-caf/msm8998/media .repo/ -delete
-git clone https://github.com/ArrowOS/android_hardware_qcom_media --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/media --depth=1
-git clone https://github.com/ArrowOS/android_hardware_qcom_audio --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/audio --depth=1
-git clone https://github.com/ArrowOS/android_hardware_qcom_display --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/display --depth=1
-git clone https://github.com/ArrowOS/android_hardware_qcom_vr --single-branch -b arrow-11.0 hardware/qcom-caf/vr --depth=1
-git clone -b lineage-18.1 https://github.com/LineageOS/android_external_ant-wireless_antradio-library external/ant-wireless/antradio-library
-git clone -b arrow-11.0 https://github.com/ArrowOS/android_packages_resources_devicesettings packages/resources/devicesettings
+#find hardware/qcom-caf/msm8998/display hardware/qcom-caf/msm8998/audio hardware/qcom-caf/msm8998/media .repo/ -delete
+#git clone https://github.com/ArrowOS/android_hardware_qcom_media --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/media --depth=1
+#git clone https://github.com/ArrowOS/android_hardware_qcom_audio --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/audio --depth=1
+#git clone https://github.com/ArrowOS/android_hardware_qcom_display --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/display --depth=1
+#git clone https://github.com/ArrowOS/android_hardware_qcom_vr --single-branch -b arrow-11.0 hardware/qcom-caf/vr --depth=1
+#git clone -b lineage-18.1 https://github.com/LineageOS/android_external_ant-wireless_antradio-library external/ant-wireless/antradio-library
+#git clone -b arrow-11.0 https://github.com/ArrowOS/android_packages_resources_devicesettings packages/resources/devicesettings
 
 tg_sendText "Done... Lunching"
 
@@ -64,18 +64,18 @@ up(){
 	curl --upload-file $1 https://transfer.sh/ | tee download.txt
 }
 
-tg_sendText "ccache downlading"
-cd /tmp
-wget https://transfer.sh/OmTx4/ccache.zip
-unzip ccache.zip
-tar xf cr_ccache.tar.gz
-find cr_ccache.tar.gz ccache.zip -delete
-cd /tmp/rom
-tg_sendText "ccache done"
+#tg_sendText "ccache downlading"
+#cd /tmp
+#wget https://transfer.sh/OmTx4/ccache.zip
+#unzip ccache.zip
+#tar xf cr_ccache.tar.gz
+#find cr_ccache.tar.gz ccache.zip -delete
+#cd /tmp/rom
+#tg_sendText "ccache done"
 
 # Normal build steps
-. build/envsetup.sh
-lunch statix_lavender-userdebug
+source build/envsetup.sh
+lunch aosip_lavender-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
@@ -91,7 +91,7 @@ tg_sendText "Building"
 #make hiddenapi-lists-docs
 #tg_sendText "metalava done"
 
-brunch statix_lavender-userdebug
+timeout 60m m kronic
 
 tg_sendText "ccache"
 cd /tmp
