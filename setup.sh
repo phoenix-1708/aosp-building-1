2e051ecb9528dd577dd9cdd599da60b6c1f1f212
makaramhk <30686963+makaramhk@users.noreply.github.com>
2021-03-24 17:09:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,9 +13,10 @@ function tg_sendFile() {
 curl -F chat_id=$CHAT_ID -F document=@${1} -F parse_mode=markdown https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 }
 
+
 sudo apt-get install bc
-MANIFEST=git://github.com/AOSiP/platform_manifest.git
-BRANCH=ten
+MANIFEST=git://github.com/StatiXOS/android_manifest.git
+BRANCH=11
 
 mkdir -p /tmp/rom
 cd /tmp/rom
@@ -35,21 +36,21 @@ tg_sendText "Cloning repo done"
 # Sync device tree and stuffs
 tg_sendText "Cloning Device stuff"
 
-git clone https://github.com/coolhotham/dev_lav --single-branch -b aosip device/xiaomi/lavender --depth=1
-git clone https://github.com/coolhotham/ven_lav --single-branch -b ten vendor/xiaomi/lavender --depth=1
+git clone https://github.com/coolhotham/device_lav.git --single-branch -b arrow-11.0 device/xiaomi/lavender --depth=1
+git clone https://github.com/coolhotham/vendor_lav.git --single-branch -b arrow-11.0 vendor/xiaomi/lavender --depth=1
 git clone https://github.com/NotZeetaa/nexus_kernel_lavender.git -b Hmp kernel/xiaomi/lavender --depth=1
 
 tg_sendText "Done. Cloning HALs...."
 
 #cloning HALs
 # Sync stuffs
-#find hardware/qcom-caf/msm8998/display hardware/qcom-caf/msm8998/audio hardware/qcom-caf/msm8998/media .repo/ -delete
-#git clone https://github.com/ArrowOS/android_hardware_qcom_media --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/media --depth=1
-#git clone https://github.com/ArrowOS/android_hardware_qcom_audio --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/audio --depth=1
-#git clone https://github.com/ArrowOS/android_hardware_qcom_display --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/display --depth=1
-#git clone https://github.com/ArrowOS/android_hardware_qcom_vr --single-branch -b arrow-11.0 hardware/qcom-caf/vr --depth=1
-#git clone -b lineage-18.1 https://github.com/LineageOS/android_external_ant-wireless_antradio-library external/ant-wireless/antradio-library
-#git clone -b arrow-11.0 https://github.com/ArrowOS/android_packages_resources_devicesettings packages/resources/devicesettings
+find hardware/qcom-caf/msm8998/display hardware/qcom-caf/msm8998/audio hardware/qcom-caf/msm8998/media .repo/ -delete
+git clone https://github.com/ArrowOS/android_hardware_qcom_media --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/media --depth=1
+git clone https://github.com/ArrowOS/android_hardware_qcom_audio --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/audio --depth=1
+git clone https://github.com/ArrowOS/android_hardware_qcom_display --single-branch -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/display --depth=1
+git clone https://github.com/ArrowOS/android_hardware_qcom_vr --single-branch -b arrow-11.0 hardware/qcom-caf/vr --depth=1
+git clone -b lineage-18.1 https://github.com/LineageOS/android_external_ant-wireless_antradio-library external/ant-wireless/antradio-library
+git clone -b arrow-11.0 https://github.com/ArrowOS/android_packages_resources_devicesettings packages/resources/devicesettings
 
 tg_sendText "Done... Lunching"
 
@@ -58,28 +59,24 @@ tg_sendText "Done... Lunching"
 #export _JAVA_OPTIONS="-Xmx6g"
 #prebuilts/jdk/jdk9/linux-x86/bin/java -XX:+PrintFlagsFinal -version  | grep "MaxHeapSize"
 
-# upload function for uploading rom zip file! I don't want unwanted builds in my google drive haha!
 
-up(){
-	curl --upload-file $1 https://transfer.sh/ | tee download.txt
-}
 
-tg_sendText "ccache downlading"
-cd /tmp
-wget https://transfer.sh/juSxs/ccache.zip
-unzip ccache.zip
-tar xf cr_ccache.tar.gz
-find cr_ccache.tar.gz ccache.zip -delete
-cd /tmp/rom
-tg_sendText "ccache done"
+#tg_sendText "ccache downlading"
+#cd /tmp
+#wget https://transfer.sh/juSxs/ccache.zip
+#unzip ccache.zip
+#tar xf cr_ccache.tar.gz
+#find cr_ccache.tar.gz ccache.zip -delete
+#cd /tmp/rom
+#tg_sendText "ccache done"
 
 # Normal build steps
-. build/envsetup.sh
-lunch aosip_lavender-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
+. build/envsetup.sh
+lunch statix_lavender-userdebug
+export USE_CCACHE=1
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
-export USE_CCACHE=1
 ccache -M 10G
 ccache -o compression=true
 ccache -z
@@ -92,24 +89,32 @@ tg_sendText "Building"
 #mka hiddenapi-lists-docs
 #tg_sendText "metalava done"
 
-m kronic
+timeout 60m brunch statix_lavender-userdebug
 
+#compress to tar code
 com () 
 { 
     tar --use-compress-program="pigz -k -$2 " -cf cr_$1.tar.gz $1
 }
 
-up out/target/product/lavender/*.zip
-tg_sendFile "download.txt"
-#tg_sendFile "out/target/product/lavender/*.zip"
-up out/target/product/lavender/*.json
-tg_sendFile "download.txt"
-tg_sendText "Build Completed"
-
+# upload function for uploading rom zip file! I don't want unwanted builds in my google drive haha!
+up(){
+	curl --upload-file $1 https://transfer.sh/ | tee download.txt
+}
 
 tg_sendText "ccache"
 cd /tmp
-time com ccache 1 # Compression level 1, its enough
+time com ccache 3 # Compression level 1, its enough
 #zip ccache.zip cr_ccache.tar.gz
 up cr_ccache.tar.gz
 tg_sendFile "download.txt"
+
+tg_sendText "Build zip"
+cd /tmp/rom
+up out/target/product/lavender/*.zip
+tg_sendFile "download.txt"
+#tg_sendFile "out/target/product/lavender/*.zip"
+tg_sendText "json"
+up out/target/product/lavender/*.json
+tg_sendFile "download.txt"
+tg_sendText "Build Completed"
