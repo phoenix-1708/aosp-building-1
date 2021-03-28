7418c1ff7b51dcacbc90eea43138cdc85f044e47
makaramhk <30686963+makaramhk@users.noreply.github.com>
2021-03-28 17:03:06 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ up(){
 	curl --upload-file $1 https://transfer.sh/ | tee download.txt
 }
 
-sudo apt-get update -y
+#sudo apt-get update -y
 sudo apt-get install -y openjdk-11-jdk
 java -version
 sudo apt-get install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
