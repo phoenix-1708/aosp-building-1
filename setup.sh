cf0f73c4da70152b3a723baa175034b511a2ecac
makaramhk <30686963+makaramhk@users.noreply.github.com>
2021-03-28 11:15:23 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ up(){
 	curl --upload-file $1 https://transfer.sh/ | tee download.txt
 }
 
-sudo apt-get install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+sudo apt-get install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 sudo apt-get install wget
 MANIFEST=git://github.com/TenX-OS/manifest_TenX
 BRANCH=eleven
