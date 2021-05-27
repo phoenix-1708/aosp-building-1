068c60fd09c827d9eb9ae13425e6aa168514d2b5
makhk <30686963+makhk@users.noreply.github.com>
2021-05-27 11:49:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -115,8 +115,8 @@ tg_sendText "Building"
 #tg_sendText "metalava done.. Building"
 export PATH="$HOME/bin:$PATH"
 
-sleep 65m && cd /tmp && tg_sendText "ccache compress" && time com ccache 1 && tg_sendText "ccache upload" && time rclone copy cr_ccache.tar.gz hk:flos/ -P && up cr_ccache.tar.gz && tg_sendFile "download.txt" && cd /tmp/rom &
-make bacon -j$(nproc --all) || make bacon -j12
+#sleep 70m && cd /tmp && tg_sendText "ccache compress" && time com ccache 1 && tg_sendText "ccache upload" && time rclone copy cr_ccache.tar.gz hk:flos/ -P && up cr_ccache.tar.gz && tg_sendFile "download.txt" && cd /tmp/rom &
+make bacon -j12 || make bacon -j$(nproc --all)
 
 
 tg_sendText "Build zip"
